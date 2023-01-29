2067a20fc2b54689938462d46b3844fa67f3a0ae
Yun Wu <110986659+UnAbuse@users.noreply.github.com>
2023-01-30 02:32:23 +0800

    Create Proxy.reptile.py

---
+++ Proxy.reptile.py
@@ -0,0 +1,30 @@
+from packet import *
+
+class Proxy(Meth):
+	def __init__(self):
+		super(GetIp, self).__init__()
+		self.headers = {
+			'User-Agent' :'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33'
+		}
+		self.url = 'https://www.beesproxy.com/free/page/'
+		self.encoding = 'utf-8'
+
+	def run(self, page):
+		data = etree.HTML(super(GetIp, self).get_Html(
+			''.join((self.url, str(page))),
+			'GET',
+			'text',
+			self.headers,
+			self.encoding
+			))
+		ip_list = data.xpath('//*[@id="article-copyright"]/figure/table/tbody/tr/td[1]')
+		port_list = data.xpath('//*[@id="article-copyright"]/figure/table/tbody/tr/td[2]')
+		with open('ip.proxy.txt', 'a+', encoding='utf-8') as f:
+			res = list(map(lambda x, y: {
+					"http": "".join(('http://', x.xpath('string(.)').strip(), ':', y.xpath('string(.)').strip())),
+					"https": "".join(('http://', x.xpath('string(.)').strip(), ':', y.xpath('string(.)').strip()))
+				},
+				ip_list,
+				port_list
+				))
+			list(map(lambda x: f.write(''.join((str(x), '\n'))), res))
