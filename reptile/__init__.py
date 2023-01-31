8f4b59f86eb5182728db5b74fa40668af10e9b6e
叶子 <110986659+UnAbuse@users.noreply.github.com>
2023-01-31 16:11:05 +0800

    Create __init__.py

---
+++ reptile/__init__.py
@@ -0,0 +1,9 @@
+'''
+作者：UnAbuse
+githud地址：https://github.com/UnAbuse
+转载请注明出处
+'''
+from FanqieNovelReptile import *
+from BiliBiliReptile import *
+from PixivelReptile import *
+from ProxyReptile import *
