2f2d45bfdb53dc55b69d27f9c6964d9fe32d1deb
叶子 <110986659+UnAbuse@users.noreply.github.com>
2024-01-15 15:19:58 +0800

    Create config.py

---
+++ FanQieNovel/config.py
@@ -0,0 +1,15 @@
+# Cookie
+Cookie = ''
+# 搜索api
+search_url = 'https://novel.snssdk.com/api/novel/channel/homepage/search/search/v1/'
+# 请求头
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Danger hiptop 3.4; U; AvantGo 3.2)',
+    'Cookie': Cookie
+    }
+# 编码
+encoding = 'utf-8'
+# 目录api
+directory_url = 'https://novel.snssdk.com/api/novel/book/directory/list/v1/'
+# 正文api
+content_url = 'https://novel.snssdk.com/api/novel/book/reader/full/v1/'
